880a70d8bf7bce95f1a08e762b75146e7d7f6ff8
ishida <wannpi8888@icloud.com>
2018-08-21 11:39:03 +0900

    mainファイルをつくりました

---
+++ main.rb
@@ -0,0 +1,15 @@
+﻿require 'dxruby'
+
+Window.width = 1280
+Window.height = 720
+
+font = Font.new(32)
+
+x = 100
+y = 100
+
+Window.loop do
+  Window.draw_font(x, y, 'A', font)
+  x = x + 1
+  y = y + 1
+end

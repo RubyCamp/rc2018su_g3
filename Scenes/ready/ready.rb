3c93ed4deba3d1ef3b6ffc6e26290e61fb08f934
Tanaka Takehiro <saikyoutabiban@yahoo.co.jp>
2018-08-22 11:21:35 +0900

    add images(start,mouse,keyboard) title now can go ready with click start or push enterkey ready now waits click and push enterkey

--- Scenes/ready/ready.rb
+++ Scenes/ready/ready.rb
@@ -1,12 +1,28 @@
 module Ready
   class Director
     BACKGROUND = Image.load('images/ruby.png')
+    MOUSE = Image.load('images/mouse.png')
+    KEYBOARD = Image.load('images/keyboard.png')
+    RMOUSE = Image.load('images/mouseready.png')
+    RMOUSE.set_color_key([255,255,255])
+    RKEYBOARD = Image.load('images/keyboardready.png')
+    RKEYBOARD.set_color_key([255,255,255])
 def initialize
   @x=0
   @y=0
 end
     def play
       Window.draw(0, 0, BACKGROUND)
+      if @x==0
+      Window.draw(100, 200, KEYBOARD)
+    elsif @x==1
+      Window.draw(100, 200, RKEYBOARD)
+      end
+      if @y==0
+      Window.draw(700, 200, MOUSE)
+    elsif @y==1
+        Window.draw(700, 200, RMOUSE)
+      end
       if Input.key_push?(K_RETURN) then
         @x=1
       end
--- Scenes/title/title.rb
+++ Scenes/title/title.rb
@@ -1,12 +1,19 @@
 module Title
   class Director
     BACKGROUND = Image.load('images/mog05_1.png')
-
+    START = Image.load('images/start.png')
     def play
     	Window.draw(0, 0, BACKGROUND)
+      Window.draw(400,550,START)
 
-      Scene.current = :ready if Input.key_push?(K_RETURN)
-
+      if Input.mouse_x >= 400 && Input.mouse_x <= 758 && Input.mouse_y >= 550 && Input.mouse_y <= 654
+        if Input.mouse_down?(M_LBUTTON)
+          Scene.current = :ready
+        end
+      end
+      if Input.key_push?(K_RETURN)
+        Scene.current = :ready
+      end
     end
   end
 end
